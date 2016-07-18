bbe9e41c13eb45fdb3796b91dcefa716b93d966d
150Balbes <Leo-sp150@yandex.ru>
2016-07-18 11:45:46 +0300

    del old files

--- arch/arm/mach-meson/Makefile.boot
+++
@@ -1,4 +0,0 @@
-   zreladdr-y	:= 0x00208000
-params_phys-y	:= 0x00200100
-initrd_phys-y	:= 0x00a00000
-
--- include/linux/hardkernel/odroidc2.h
+++
@@ -1,8 +0,0 @@
-#ifndef __ODROIDC2_H
-#define __ODROIDc2_H
-
-int board_boot_from_emmc(void);
-
-char *efuse_get_mac(char *addr);
-
-#endif
